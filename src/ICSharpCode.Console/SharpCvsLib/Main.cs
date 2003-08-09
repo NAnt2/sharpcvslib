8a397e786aca5b54e7db525ba843a5559ef56fca
drakmar <drakmar>
2003-08-09 21:57:10 +0000

    Change to usage statement for console application.

--- src/ICSharpCode.Console/SharpCvsLib/Main.cs
+++ src/ICSharpCode.Console/SharpCvsLib/Main.cs
@@ -5,7 +5,31 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello World!");
+        if (args.Length < 1) {
+            System.Console.WriteLine (Usage);
+        }
+    }
+    
+    private static String Usage {
+        get {
+            return  
+@"   Usage: cvs [cvs-options] command [command-options-and-arguments]
+      where cvs-options are -q, -n, etc.
+        (specify --help-options for a list of options)
+      where command is add, admin, etc.
+        (specify --help-commands for a list of commands
+         or --help-synonyms for a list of command synonyms)
+      where command-options-and-arguments depend on the specific command
+        (specify -H followed by a command name for command-specific help)
+      Specify --help to receive this message
+    
+    The Concurrent Versions System (CVS) is a tool for version control.
+    For CVS updates and additional information, see
+        the #CvsLib home page at http://sharpcvslib.sourceforge.net/ or
+        the CVS home page at http://www.cvshome.org/ or
+        Pascal Molli's CVS site at http://www.loria.fr/~molli/cvs-index.html
+        the CVSNT home page at http://www.cvsnt.org/
+        ";
+        }
     }
 }
