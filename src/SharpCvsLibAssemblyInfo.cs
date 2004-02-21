b7da0110d4db40c01a9ef40d7d827f5c1c870271
drakmar <drakmar>
2004-02-21 09:49:20 +0000

    *** empty log message ***

--- src/SharpCvsLibAssemblyInfo.cs
+++ src/SharpCvsLibAssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
-[assembly: AssemblyVersionAttribute("0.35.3721.417")]
+[assembly: AssemblyVersionAttribute("0.35.3721.507")]
 [assembly: AssemblyTitleAttribute("Concurrent Versions System (sharpCVS) (client)")]
-[assembly: AssemblyDescriptionAttribute("win32; net-1.0; 2004/02/21 01:09:39")]
+[assembly: AssemblyDescriptionAttribute("win32; net-1.0; 2004/02/21 01:24:31")]
 [assembly: AssemblyCopyrightAttribute("Mike Krueger 2001-2002, SharpCvsLib Developers 2003-2004\r\n\r\nCVS may be copied onl" +
 "y under the terms of the GNU General Public License,\r\na copy of which can be fou" +
 "nd with the CVS distribution kit.")]
