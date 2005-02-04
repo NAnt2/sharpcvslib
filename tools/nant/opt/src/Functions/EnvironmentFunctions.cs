0761e949196f3180e2d9fbfcd06be691e1f46b8f
drakmar <drakmar>
2005-02-04 04:02:58 +0000

    Removed nant extras documentation.  Removed WinCE from the platform options.

--- tools/nant/opt/src/Functions/EnvironmentFunctions.cs
+++ tools/nant/opt/src/Functions/EnvironmentFunctions.cs
@@ -51,8 +51,7 @@ namespace Sporadicism.NAntExtras.Functions {
         public static string Start(string varName, string varValue) {
             if (Environment.OSVersion.Platform == PlatformID.Win32NT ||
                 Environment.OSVersion.Platform == PlatformID.Win32S ||
-                Environment.OSVersion.Platform == PlatformID.Win32Windows ||
-                Environment.OSVersion.Platform == PlatformID.WinCE) {
+                Environment.OSVersion.Platform == PlatformID.Win32Windows) {
                 SetEnvironmentVariable(varName, varValue);            
             } else {
                 setenv(varName, varValue, 0);
